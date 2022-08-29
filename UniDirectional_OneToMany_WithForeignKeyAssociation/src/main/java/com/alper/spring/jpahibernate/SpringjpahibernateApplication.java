07cff0b651cdb0274dd45c870089e7617a56cc64
Alper Dönmez <44973133+nemokayipolmus@users.noreply.github.com>
2022-08-29 13:51:44 +0300

    Add files via upload

---
+++ UniDirectional_OneToMany_WithForeignKeyAssociation/src/main/java/com/alper/spring/jpahibernate/SpringjpahibernateApplication.java
@@ -0,0 +1,16 @@
+package com.alper.spring.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpahibernateApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(SpringjpahibernateApplication.class, args);
+
+
+
+    }
+
+}
---
+++ UniDirectional_OneToMany_WithForeignKeyAssociation/src/main/java/com/alper/spring/jpahibernate/model/UniDirectional_OneToMany_WithForeignKeyAssociation/Branch.java
@@ -0,0 +1,25 @@
+package com.alper.spring.jpahibernate.model.UniDirectional_OneToMany_WithForeignKeyAssociation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+}
